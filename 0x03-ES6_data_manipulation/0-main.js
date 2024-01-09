c79751dcd64fe2f09a1a170e82d6834b481464e8
Techkene <keneanoliefo24@gmail.com>
2024-01-09 19:32:57 +0100

    update all

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -1,5 +1,4 @@
-[
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
+/* eslint-disable import/extensions */
+import getListStudents from './0-get_list_students.js';
+
+console.log(getListStudents());
